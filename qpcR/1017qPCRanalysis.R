fdc57100739254226d8c3d221e47eb690b121333
tbillman <trb9259@uncw.edu>
2017-10-19 13:04:27 -0400

    updated

--- qpcR/1017qPCRanalysis.R
+++ qpcR/1017qPCRanalysis.R
@@ -15,7 +15,6 @@ fef = lapply(freps,function(x){
 })
 fusing <-list('A5','C5','D1','E5','F1','G5','G9','H1',
                  'I5','I9','J1','K5','K9','L1','P5','P9')
-)
 
 h1017<- read.csv(file = "C:/Users/Thomas/Documents/PCR data/1017qPCR/hex1017.csv", header = TRUE)
 h1017[,1] = NULL
@@ -32,11 +31,50 @@ hef = lapply(hreps,function(x){
 })
 
 husing = list('A1','A9','A13','A17','B5','C1','C9','C13','C17','D5','E1','E9','E13','E17','F5',
-              'G1','G9','G13','G17','H5','I1','I9','I13','I17','J5','K1','K9','L5','P1','P9'
-  
-)
+              'G1','G9','G13','G17','H5','I1','I9','I13','I17','J5','K1','K9','L5','P1','P9')
+
 shared = setdiff(husing,setdiff(husing,fusing))
-which(husing) = shared[1]
-ratios = lapply(1:length(hef), function(x){
-  hef[[x]]$cpD1/fef[[x]]$cpD1
+#which runs in hex are shared 
+hshared = hef[which(is.na(match(husing,shared)) == FALSE)]
+fshared = fef[which(is.na(match(fusing,shared)) == FALSE)]
+ratios = lapply(1:length(hshared), function(x){
+  hshared[[x]]$cpD1-fshared[[x]]$cpD1
 })
+unlist(ratios)
+feff = lapply(m,function(x){
+  efficiency(x,plot = FALSE) 
+})
+heff= lapply(hm,function(x){
+  efficiency(x,plot = FALSE)
+})
+ratiosf = lapply(1:length(feff), function(x){
+   c(heff[[x]]$cpD1-feff[[x]]$cpD1,heff[[x]]$cpD2-feff[[x]]$cpD2)
+})
+#####fam cpd1 vector#####
+fcpd1 = vector(); fcpd1 = lapply(1:length(feff),function(x){
+  fcpd1 = c(fcpd1, feff[[x]]$cpD1)
+})
+fcpd1 = unlist(fcpd1)
+#####fam cpd2 vector#####
+fcpd2 = vector(); fcpd2 = lapply(1:length(feff),function(x){
+  fcpd2 = c(fcpd2, feff[[x]]$cpD2)
+})
+fcpd2 = unlist(fcpd2)
+#####hex cpd1 vector#####
+hcpd1 = vector(); hcpd1 = lapply(1:length(heff),function(x){
+  hcpd1 = c(hcpd1, heff[[x]]$cpD1)
+})
+hcpd1 = unlist(hcpd1)
+#####hex cpd2 vector#####
+hcpd2 = vector(); hcpd2 = lapply(1:length(heff),function(x){
+  hcpd2 = c(hcpd2, heff[[x]]$cpD2)
+})
+hcpd2 = unlist(hcpd2)
+
+eff.frame = as.data.frame(ratiosf, col.names = colnames(f1017)[-1], row.names = c("cpD1rat","cpD2rat"))
+eff.frame = rbind(fcpd1,fcpd2,hcpd1,hcpd2,eff.frame)
+row.names(eff.frame) = c("FAM cpD1", "FAM cpD2",
+              "HEX cpD1", "HEX cpD2",
+              "cpD1 ratio", "cpD2 ratio")
+
+write.csv(eff.frame, file = "Github/Stapleton-Lab/qPCR/1017ratios.csv")
