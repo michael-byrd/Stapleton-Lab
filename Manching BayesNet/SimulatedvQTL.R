87f3e93d07299e79242988d5391cade0f5f81433
Michael Byrd <mbyrdsmith94@gmail.com>
2018-06-05 10:42:34 -0400

    Changed file reference in Simulated R File to Stampede path

--- Manching BayesNet/SimulatedvQTL.R
+++ Manching BayesNet/SimulatedvQTL.R
@@ -1,7 +1,12 @@
 #vQTL with simulated data
 library("qtl")
 library("vqtl")
-dat <- read.cross(file = "C:/Users/Thomas/Documents/GitHub/Stapleton-Lab/Manching\ BayesNet/SimulatedResponse.csv")
+
+# Michael Stampede Path
+dat <- read.cross(file = "/work/04908/mcb4548/stampede2/GitHub/Thomas_Code_Forked/Stapleton-Lab/Manching\ BayesNet/SimulatedResponse.csv")
+# Thomas Git Path
+# dat <- read.cross(file = "C:/Users/Thomas/Documents/GitHub/Stapleton-Lab/Manching\ BayesNet/SimulatedResponse.csv")
+
 dat <- drop.nullmarkers(dat)
 #scan with variance
 dat <- calc.genoprob(dat)
@@ -9,11 +14,11 @@ outv <- scanonevar(cross = dat,
                     mean.formula = Height ~ Low.Water + Low.Nitrogen + Pathogen + mean.QTL.add + mean.QTL.dom,
                     var.formula = ~ var.QTL.add + var.QTL.dom)
 library("dplyr")
-effect.sizes = function (cross, phenotype.name, focal.groups = NULL, nuisance.groups = NULL, 
-                         genotype.names = c("AA", "AB", "BB"), xlim = NULL, ylim = NULL, 
-                         title = paste(phenotype.name, "by", paste(focal.groups, 
-                                                                   collapse = ", ")), draw_ribbons = TRUE, se_line_size = 1, 
-                         point_size = 1) 
+effect.sizes = function (cross, phenotype.name, focal.groups = NULL, nuisance.groups = NULL,
+                         genotype.names = c("AA", "AB", "BB"), xlim = NULL, ylim = NULL,
+                         title = paste(phenotype.name, "by", paste(focal.groups,
+                                                                   collapse = ", ")), draw_ribbons = TRUE, se_line_size = 1,
+                         point_size = 1)
 {
   indiv.mean.estim <- indiv.mean.lb <- indiv.mean.ub <- "fake_global_for_CRAN"
   indiv.sd.estim <- indiv.sd.lb <- indiv.sd.ub <- "fake_global_for_CRAN"
@@ -21,12 +26,12 @@ effect.sizes = function (cross, phenotype.name, focal.groups = NULL, nuisance.gr
   group.sd.estim <- group.sd.ub <- group.sd.lb <- "fake_global_for_CRAN"
   modeling.df <- dplyr::data_frame(placeholder = rep(NA, qtl::nind(cross)))
   modeling.df[[phenotype.name]] <- cross[["pheno"]][[phenotype.name]]
-  marker.names <- c(focal.groups[focal.groups %in% colnames(qtl::pull.geno(cross = cross))], 
+  marker.names <- c(focal.groups[focal.groups %in% colnames(qtl::pull.geno(cross = cross))],
                     nuisance.groups[nuisance.groups %in% colnames(qtl::pull.geno(cross = cross))])
-  phen.names <- c(focal.groups[focal.groups %in% colnames(qtl::pull.pheno(cross = cross))], 
+  phen.names <- c(focal.groups[focal.groups %in% colnames(qtl::pull.pheno(cross = cross))],
                   nuisance.groups[nuisance.groups %in% colnames(qtl::pull.pheno(cross = cross))])
   for (marker.name in marker.names) {
-    modeling.df[[marker.name]] <- factor(x = qtl::pull.geno(cross = cross)[, 
+    modeling.df[[marker.name]] <- factor(x = qtl::pull.geno(cross = cross)[,
                                                                            marker.name], labels = genotype.names)
   }
   for (phen.name in phen.names) {
@@ -35,12 +40,12 @@ effect.sizes = function (cross, phenotype.name, focal.groups = NULL, nuisance.gr
   modeling.df[["placeholder"]] <- NULL
   covar.form <- paste(focal.groups, collapse = "+")
   if (!is.null(nuisance.groups)) {
-    covar.form <- paste(covar.form, "+", paste(nuisance.groups, 
+    covar.form <- paste(covar.form, "+", paste(nuisance.groups,
                                                collapse = "+"))
   }
   mean.form <- paste(phenotype.name, "~", covar.form)
   var.form <- paste("~", covar.form)
-  dglm.fit <- dglm::dglm(formula = stats::formula(mean.form), 
+  dglm.fit <- dglm::dglm(formula = stats::formula(mean.form),
                          dformula = stats::formula(var.form), data = modeling.df)
   mean.pred <- stats::predict(dglm.fit, se.fit = TRUE)
   mean.estim <- mean.pred$fit
@@ -48,15 +53,15 @@ effect.sizes = function (cross, phenotype.name, focal.groups = NULL, nuisance.gr
   sd.pred <- stats::predict(dglm.fit$dispersion.fit, se.fit = TRUE)
   sd.estim <- sd.pred$fit/sd.pred$residual.scale
   sd.se <- sd.pred$se.fit
-  indiv.prediction.tbl <- dplyr::bind_cols(stats::na.omit(modeling.df), 
-                                           dplyr::data_frame(indiv.mean.estim = mean.estim, indiv.mean.lb = mean.estim - 
-                                                               mean.se, indiv.mean.ub = mean.estim + mean.se, indiv.sd.estim = exp(sd.estim), 
-                                                             indiv.sd.lb = exp(sd.estim - sd.se), indiv.sd.ub = exp(sd.estim + 
+  indiv.prediction.tbl <- dplyr::bind_cols(stats::na.omit(modeling.df),
+                                           dplyr::data_frame(indiv.mean.estim = mean.estim, indiv.mean.lb = mean.estim -
+                                                               mean.se, indiv.mean.ub = mean.estim + mean.se, indiv.sd.estim = exp(sd.estim),
+                                                             indiv.sd.lb = exp(sd.estim - sd.se), indiv.sd.ub = exp(sd.estim +
                                                                                                                       sd.se)))
-  group.prediction.tbl <- indiv.prediction.tbl %>% dplyr::group_by_(.dots = c(focal.groups)) %>% 
-    dplyr::summarise(group.mean.estim = mean(indiv.mean.estim), 
-                     group.mean.lb = mean(indiv.mean.lb), group.mean.ub = mean(indiv.mean.ub), 
-                     group.sd.estim = mean(indiv.sd.estim), group.sd.lb = mean(indiv.sd.lb), 
+  group.prediction.tbl <- indiv.prediction.tbl %>% dplyr::group_by_(.dots = c(focal.groups)) %>%
+    dplyr::summarise(group.mean.estim = mean(indiv.mean.estim),
+                     group.mean.lb = mean(indiv.mean.lb), group.mean.ub = mean(indiv.mean.ub),
+                     group.sd.estim = mean(indiv.sd.estim), group.sd.lb = mean(indiv.sd.lb),
                      group.sd.ub = mean(indiv.sd.ub))
   return(group.prediction.tbl)
 }
@@ -86,7 +91,7 @@ sapply(1:length(sizedf), function(x){
 })
 nall0 <-sapply(1:dim(sizedf1)[1], function(x){
   !all(sizedf1[x,] == 0)
-})  
+})
 ditch <- which(nall0 == F)
 sizedf1 <- sizedf1[-ditch,]
 keep <- y; keep<- keep[-ditch]
