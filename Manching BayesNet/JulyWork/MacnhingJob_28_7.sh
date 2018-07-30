fb4a9f7106de77262b865d0ff97233ed0418ee81
Michael Byrd <mbyrdsmith94@gmail.com>
2018-07-29 20:48:58 -0400

    Working on vQTL Manching Data Cleaning and job running

---
+++ Manching BayesNet/JulyWork/MacnhingJob_28_7.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH -J sim_vQTL_29_July           # Job name
+#SBATCH -e myjob.e%j       # Name of stderr error file
+#SBATCH -p skx-normal          # Queue (partition) name
+#SBATCH -N 1               # Total # of nodes (must be 1 for serial)
+#SBATCH -n 1               # Total # of mpi tasks (should be 1 for serial)
+#SBATCH -t 48:00:00        # Run time (hh:mm:ss)
+#SBATCH --mail-user=mcb4548@uncw.edu
+#SBATCH --mail-type=all    # Send email at begin and end of job
+
+
+# Other commands must follow all #SBATCH directives...
+
+mkdir output
+Rscript --vanilla --verbose ./SimulatedvQTL.R > ./output.Rout
---
+++ Manching BayesNet/JulyWork/Manching_Small_Data.R
@@ -0,0 +1,15 @@
+setwd("/Users/mbyrd/StapletonLab/Thomas/Stapleton-Lab/Manching BayesNet")
+dat <- read.csv(file = "ManchingScrubbed.csv")
+
+small_dat <- dat[sample(4:6675, 5),]
+
+small_dat <- rbind(dat[1:3,],small_dat)
+
+small_dat[1,]
+
+# col_samp <- small_dat[, sample(5:3239, 100)]
+# 
+# 
+# small_dat <- cbind(small_dat[1:4],col_samp)
+
+write.csv(small_dat, "small_manching.csv")
